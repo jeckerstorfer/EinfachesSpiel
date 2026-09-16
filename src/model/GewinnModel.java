9539c5b83d462d289588b275b3a7d8cdb6794a59
Julius <juliuseckerstorfer@gmail.com>
2026-09-16 13:40:18 +0200

    GewinnModel erstellt mit Gettern und Konstruktor

---
+++ src/model/GewinnModel.java
@@ -0,0 +1,27 @@
+package model;
+
+public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int runderErgebniss;
+
+    public GewinnModel() {
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getSpielerZahl() {
+        return spielerZahl;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRunderErgebniss() {
+        return runderErgebniss;
+    }
+}
