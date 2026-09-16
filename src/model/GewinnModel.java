ba7ee0b657fd35b8834a4fb21da57df80b35821a
Julius <juliuseckerstorfer@gmail.com>
2026-09-16 16:17:19 +0200

    Logik von GewinnModel fertig

--- src/model/GewinnModel.java
+++ src/model/GewinnModel.java
@@ -4,9 +4,13 @@ public class GewinnModel {
     private int gesamtPunkte;
     private int spielerZahl;
     private int computerZahl;
-    private int runderErgebniss;
+    private int rundenErgebnis;
 
     public GewinnModel() {
+        this.gesamtPunkte = 0;
+        this.spielerZahl = 30;
+        this.computerZahl = 0;
+        this.rundenErgebnis = 0;
     }
 
     public int getGesamtPunkte() {
@@ -21,7 +25,41 @@ public class GewinnModel {
         return computerZahl;
     }
 
-    public int getRunderErgebniss() {
-        return runderErgebniss;
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
+
+    public void berechneComputerZahl() {
+        this.computerZahl = (int) ((Math.random() * 9) + 1);
+    }
+
+    public void berechneRunde(int spielerZahl) {
+        if(spielerZahl > 9 || spielerZahl < 1) {
+            return;
+        }
+        int differenz = this.spielerZahl - this.computerZahl;
+        if(differenz == 0) {
+            rundenErgebnis = 20;
+        }
+        if(differenz == 1 || differenz == -1) {
+            rundenErgebnis = 5;
+        } else {
+            rundenErgebnis = -10;
+        }
+        this.gesamtPunkte += this.rundenErgebnis;
+    }
+
+    public boolean hatGewonnen() {
+        if(gesamtPunkte >= 100) {
+            return true
+        }
+        return false;
+    }
+
+    public boolean hatVerloren() {
+        if(gesamtPunkte <= 0) {
+            return true;
+        }
+        return false;
     }
 }
